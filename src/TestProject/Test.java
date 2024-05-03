f82d23b442649dc3bb66c7f035ce07aac5e8c588
anami <anamika95730@gmail.com>
2024-05-03 20:29:47 +0530

    committing my git project

---
+++ src/TestProject/Test.java
@@ -0,0 +1,17 @@
+package TestProject;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String hungry ="No";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something....");
+		}
+		else
+		{
+			System.out.println("do work...");
+		}
+	}
+
+}
